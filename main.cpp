ef9d48e552fb21bf713f45ff9a511c7b258dc7d4
Arkin Solomon <arkinsolomon@gmail.com>
2021-09-14 22:31:55 -0400

    Flash onboard and external (pin 28) LED alternating.

--- main.cpp
+++ main.cpp
@@ -3,27 +3,33 @@
 
 int main()
 {
+  //The delay between flashes (in milliseconds)
+  int delay = 500;
 
   //The pin number
-  const uint led_pin = 28;
+  const uint external_led_pin = 28;
+  const uint internal_led_pin = 25;
 
   //Initialize LED pin
-  gpio_init(led_pin);
+  gpio_init(external_led_pin);
+  gpio_init(internal_led_pin);
 
   //Set the direction of the data
-  gpio_set_dir(led_pin, GPIO_OUT);
-
+  gpio_set_dir(external_led_pin, GPIO_OUT);
+  gpio_set_dir(internal_led_pin, GPIO_OUT);
+  
   //Initialize chosen serial port
   stdio_init_all();
 
   //Loop forever
   while (true)
   {
-
     //Blink LED
-    gpio_put(led_pin, true); //Put true for on
-    sleep_ms(1000);
-    gpio_put(led_pin, false); //Put false for off
-    sleep_ms(1000);
+    gpio_put(external_led_pin, true); //Put true for on
+    gpio_put(internal_led_pin, false);
+    sleep_ms(delay);
+    gpio_put(external_led_pin, false); //Put false for off
+    gpio_put(internal_led_pin, true);
+    sleep_ms(delay);
   }
 }
