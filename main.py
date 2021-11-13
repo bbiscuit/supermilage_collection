e7164a0eaa9393e183fb9ac6b0c07273f760f365
ArkinSolomon <arkin@arkinsolomon.net>
2021-11-13 10:45:42 -0500

    Print a lot more

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from src.display import Display
 # Create a basic display
 display = Display()
 
-for i in range(0, 15):
+for i in range(0, 100000):
     display.print_text("Line #" + str(i))
+    print("Hello, this is line #" + str(i))
